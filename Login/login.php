47a537eb0a4a3e3600d740946ddcdea6f4f703e6
Gustavo <rosalesagustavo@gmail.com>
2023-05-03 10:43:54 -0400

    redirect to register

--- Login/login.php
+++ Login/login.php
@@ -3,6 +3,13 @@ session_start();
 $config = parse_ini_file('../private/config.ini');
 define('DB_hostname', 'localhost');
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['btnRegister'])) {
+        header("Location: ../register.php");
+        exit();
+    }
+}
+
 $connection = mysqli_connect(DB_hostname, $config['username'], $config['password'], $config['dbname']);
 
 if ($connection->connect_error) {
@@ -10,10 +17,11 @@ if ($connection->connect_error) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
     $username = $_POST['username'] ?? '';
     $password = $_POST['password'] ?? '';
 
-    $stmt = $connection->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt = $connection->prepare("SELECT * FROM users WHERE board_username = ?");
     $stmt->bind_param("s", $username);
     $stmt->execute();
 
@@ -23,9 +31,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['username'] = $username;
         // redirects to dashboard.php
-        header("Location: dashboard.php");
+        header("Location: ../dashboard.php");
         // Make sure that code below does not get executed when redirect
-        exit;
+        exit();
     }
     else {
         $error_message = "Invalid username or password!";
@@ -50,13 +58,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <h1 class="header">Message Board</h1>
             <form action="login.php" method="POST">
                 <div class="input_field">
-                    <input type="text" name="username" id="username" placeholder="Username" required="required" />
+                    <input type="text" name="username" id="username" placeholder="Username" />
                 </div>
                 <div class="input_field">
-                    <input type="password" name="password" id="password" placeholder="Password" required="required" />
+                    <input type="password" name="password" id="password" placeholder="Password" >
                 </div>
                 <input type="submit" value="Log In" />
+                <input type="submit" name="btnRegister" value="Register"/>
             </form>
+            <?php 
+                if (isset($error_message))
+                    echo '<p id="error">' . $error_message . '</p>';
+            ?>
         </div>
     </div>
 </body>

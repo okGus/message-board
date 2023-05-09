04b67d622842db5d7b52fe55378009585871cbd7
Gustavo <rosalesagustavo@gmail.com>
2023-05-09 19:36:17 -0400

    getting data from db

--- src/Dashboard/dashboard.php
+++ src/Dashboard/dashboard.php
@@ -1,1 +1,33 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header('Location: ../Login/login.php');
+    exit();
+}
+
+$username = $_SESSION['username'];
+echo "<script>console.log(" . json_encode($username) . ")</script>";
+
+$config = parse_ini_file('../../private/config.ini');
+define('DB_hostname', 'localhost');
+
+$connection = mysqli_connect(DB_hostname, $config['username'], $config['password'], $config['dbname']);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+$sql = "SELECT board_username, post.title, post.body, post.date_time FROM users INNER JOIN post ON users.userid = post.userid";
+$response = $connection->query($sql);
+
+if ($response->num_rows > 0) {
+    while ($row = $response->fetch_assoc()) {
+        echo "<script>console.log(" . json_encode($row) . ")</script>";
+    }
+}
+
+$connection->close();
+?>
+
 <h1> Dashboard </h1>
